56830bd18c12fd0feee7fe77536cdd1c3d5815d3
SodiumN1trate <renarsgausins21@gmail.com>
2021-03-16 15:09:11 +0200

    4.uzdevums

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here.
+document.addEventListener('DOMContentLoaded', function () {
+
+    const elements = document.querySelectorAll('.toggle_me')
+    const button = document.querySelector('#button')
+
+    var toggleStatus = false
+    button.addEventListener('click', function(e) {
+        if (toggleStatus == false)
+        {
+            elements.forEach(function(element) {
+                element.style.display = 'initial'
+                toggleStatus = true
+            })
+        }
+        else
+        {
+            elements.forEach(function (element) {
+                element.style.display = 'none'
+                toggleStatus = false
+            })
+        }
+    })
+
+})
