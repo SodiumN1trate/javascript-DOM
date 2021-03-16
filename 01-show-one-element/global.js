275fb327648ca0092b9abd46085603934f6db78d
SodiumN1trate <renarsgausins21@gmail.com>
2021-03-16 14:35:37 +0200

    1. uzdevums + atbildes uz jautjumiem

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -9,3 +9,18 @@ window.addEventListener("load", function(){
       button.style.display = "none"
   })
 });
+
+
+
+/*
+  Atbildes uz jautājumiem
+
+
+1. Pēc lapas restartēšanas teksts atkal būs paslēpts, jo restartējot lapu skripts arī restartējas.
+
+2. Skripts nestrādās, jo vienkārši JS neredzēs visas html izveidotās klases/id jo būs ātrāk ielādējies nekā html.
+
+3. Metode kas gaida signālu dokumenta par izpildītu eventu.
+
+
+*/
