fcc597a67dc6fbecf11e811a48e4cf09a2ffdeec
SodiumN1trate <renarsgausins21@gmail.com>
2021-03-16 16:01:09 +0200

    6. uzdevums

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,9 @@
-// Your JavaScript goes here.
+document.addEventListener('DOMContentLoaded', function () {
+
+    var htmlName = document.querySelector('#greeting')
+    const nameBar = document.querySelector('#full_name')
+    nameBar.addEventListener('keyup', function (e) {
+        htmlName.textContent = "Hello there, " + e.target.value + "!"
+    })
+
+})
