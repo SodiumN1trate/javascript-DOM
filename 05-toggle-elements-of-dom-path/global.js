70b6625f2add28de73c548e08b0fd544cf8a1c47
SodiumN1trate <renarsgausins21@gmail.com>
2021-03-16 15:43:58 +0200

    5.uzdevums

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,22 @@
-// Your JavaScript goes here.
+document.addEventListener('DOMContentLoaded', function () {
+
+    const elements = document.querySelectorAll(".second_five > li")
+    const button = document.querySelector('#toggle_button')
+
+    var toggleStatus = false
+    button.addEventListener('click', function (e) {
+        if (toggleStatus == false) {
+            elements.forEach(function (element) {
+                element.style.display = 'flex'
+                toggleStatus = true
+            })
+        }
+        else {
+            elements.forEach(function (element) {
+                element.style.display = 'none'
+                toggleStatus = false
+            })
+        }
+    })
+
+})
